3316327939af8e9b55f5f02a616e427f004cc6b5
khoo <auld2002@gmail.com>
2025-03-28 13:37:18 +0800

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            string str = obj.getSum(1, 2, 3, 4, 5, 6);
+            Console.WriteLine(str);
+            int[] arr = { 2, 4, 6, 8, 10 };
+            string str2 = obj.getSum(arr);
+            Console.WriteLine(str2);
+            Console.ReadLine();
+        }
+        public string getSum(params int[] arr)
+        {
+            int sum = 0;
+            string str = " ";
+            foreach (int i in arr)
+            {
+                sum += i;
+                str += "+" + i + " ";
+            }
+            str = str.Trim('+');
+            str += "=" + sum;
+            return str;
+        }
+    }
+
+}
+
+ 111111111111111111111
+    11111111111
+    111111111111
+    11111111
+    111111111111
